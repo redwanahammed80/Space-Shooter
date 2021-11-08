a705523b06c1726e666ed9ca9f0aa2fca14b8256
Redwan Ahammed <redwan.ahammed80@gmail.com>
2021-11-08 17:51:49 +0600

    added assets

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _speedMultiplier = 2;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _trippleShotPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+    private SpawnManager _spawnManager;
+
+    
+    private bool _isTrippleShotActive = false;
+    private bool _isSpeedBoostActive = false;
+    private bool _isShieldsActive = false;
+
+    [SerializeField]
+    private GameObject _shieldVisualizer;
+
+    [SerializeField]
+    private GameObject _leftEngine, _rightEngine;
+
+    [SerializeField]
+    private int _score;
+
+    private UIManager _uiManager;
+    [SerializeField]
+    private AudioClip _laserSoundClip;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        _audioSource = GetComponent<AudioSource>();
+
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The spawn manager is null");
+        }
+
+        if(_uiManager == null)
+        {
+            Debug.LogError("UI is null");
+        }
+
+        if(_audioSource == null)
+        {
+            Debug.LogError("AudioSource on the  is NULL");
+        }
+        else
+        {
+            _audioSource.clip = _laserSoundClip;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculatemMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculatemMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+   
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+
+        if(_isTrippleShotActive ==  true)
+        {
+            Instantiate(_trippleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+
+        }
+
+        _audioSource.Play();
+    }
+
+    public void Damage()
+    {
+        if(_isShieldsActive == true)
+        {
+            _isShieldsActive = false;
+            _shieldVisualizer.SetActive(false);
+            return;
+        }
+
+        _lives--;
+
+        if(_lives == 2)
+        {
+            _leftEngine.SetActive(true);
+        }
+
+        else if(_lives == 1)
+        {
+            _rightEngine.SetActive(true);
+        }
+
+        _uiManager.UpdateLives(_lives);
+
+        if(_lives <1)
+        {
+            _spawnManager.onPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TripleShotActive()
+    {
+        _isTrippleShotActive = true;
+        StartCoroutine(TrippleShotPowerDownRoutine());
+    }
+
+    IEnumerator TrippleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isTrippleShotActive = false;
+    }
+
+    public void SpeedBoostActive()
+    {
+        _isSpeedBoostActive = true;
+        _speed *= _speedMultiplier;
+        StartCoroutine(SpeedBoostPowerDownRoutine());
+    }
+
+    IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isSpeedBoostActive = false;
+        _speed /= _speedMultiplier;
+    }
+
+    public void ShieldsActive()
+    {
+        _isShieldsActive = true;
+        _shieldVisualizer.SetActive(true);
+    }
+
+    public void AddScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdateScore(_score);
+    }
+}
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Image _LivesImage;
+    [SerializeField]
+    private Sprite[] _livesSprites;
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreText.text = "Score: " + 50;
+        _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+    }
+
+   public void UpdateScore(int playerScore)
+    {
+        _scoreText.text = "Score" + playerScore;
+    }
+
+    public void UpdateLives(int CurrentLives)
+    {
+        _LivesImage.sprite = _livesSprites[CurrentLives];
+
+        if(CurrentLives == 0)
+        {
+            GameOverSequence();
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "  ";
+            yield return new WaitForSeconds(0.5f);
+        }
+       
+    }
+}
